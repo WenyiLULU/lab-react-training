3dd3d7651e07e8db179e922b6df2d23d65166ea7
WenyiLULU <luwenyi1991@gmail.com>
2022-06-13 16:55:53 +0200

    iteration 1 done

---
+++ src/Components/IdCard.js
@@ -0,0 +1,22 @@
+function toMeter(heightInCm) {
+    return heightInCm / 100;
+}
+function dateToString(date){
+    return date.toString().slice(0,15)
+}
+function IdCard({person}){
+    return(
+        <div className="idCard">
+            <div><img src={person.picture} alt="profile" /></div>
+            <div className="idInfo">
+                <p><strong>First name: </strong>{person.firstName}</p>
+                <p><strong>Last name: </strong>{person.lastName}</p>
+                <p><strong>Gender: </strong>{person.gender}</p>
+                <p><strong>Height: </strong>{toMeter(person.height)}m</p>
+                <p><strong>Birth: </strong>{dateToString(person.birth)}m</p>
+            </div>
+        </div>
+    )
+}
+
+export default IdCard
